94fba4dc2c320c44d565ef191b2dfe8b79ee9798
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-04-09 02:25:30 +0000

    Declare basic game functions and variables

---
+++ assets/js/game.js
@@ -0,0 +1,74 @@
+// Question bank
+let questions = [{
+    question: 'what is 2 + 1',
+    choice1: '5',
+    choice2: '4',
+    choice3: '3',
+    choice4: '21',
+    answer: 3
+},
+{
+    question: 'what is 2 + 2',
+       choice1: '5',
+       choice2: '4',
+       choice3: '9',
+       choice4: '21',
+       answer: 4
+   },
+   {
+    question: 'what is 2 + 3',
+       choice1: '5' ,
+       choice2: '4',
+       choice3: '35',
+       choice4: '21',
+       answer: 5
+   },
+   {
+    question: 'what is 2 + 4',
+       choice1: '5',
+       choice2: '4',
+       choice3: '6',
+       choice4: '21',
+       answer: 6
+   }
+];
+
+//Declarations
+let questionIndex;
+let score;
+let questionCounter;
+let availableQuestions = [];
+
+/**Main quiz function. Initializes input parameters and triggers the runQuiz function*/
+function startQuiz() {
+    questionIndex = 0;
+    score = 0;
+    questionCounter = 0;
+    availableQuestions = [...questions]
+    runQuiz()
+}
+
+/**Function to handles the running of the game */
+function runQuiz() {}
+
+/**Function to display quiz progression*/
+function displayQuizProgress(){
+}
+
+
+/**Function to pick question at random from the question's bank*/
+function getRandomQuestion () {
+}
+
+/**Function to handle player's choice */
+function makeChoice(){
+}
+
+/**
+ * A function that accepts a constant @param {scorePoint}, increases the score by the scorePoint 
+ * and sets the most recent score to the local storage
+ */
+function incrementScore(num) {
+     }
+
+startQuiz()
